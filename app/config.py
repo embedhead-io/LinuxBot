e055fe81f2fad9ff58e5908d3d19f3e70fe78f8c
Josh <josh@opaliq.com>
2023-09-03 17:35:38 -0400

    changed default model to SLOW

--- app/config.py
+++ app/config.py
@@ -10,17 +10,15 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 # OpenAI Model
 OPENAI_MODELS = [
     "gpt-4-0613",
-    "gpt-4",
     "gpt-3.5-turbo-16k-0613",
-    "gpt-3.5-turbo-16k",
 ]
 FAST_MODEL = "gpt-3.5-turbo-16k-0613"
 SLOW_MODEL = "gpt-4-0613"
-DEFAULT_MODEL = FAST_MODEL
+DEFAULT_MODEL = SLOW_MODEL
 
 # OpenAI Configurations
 OPENAI_RETRY_LIMIT = 3
-OPENAI_TEMPERATURE = 0.7
+OPENAI_TEMPERATURE = 0.8
 
 # Chat Log Path
 CHAT_LOG_PATH = "chat_history.json"
