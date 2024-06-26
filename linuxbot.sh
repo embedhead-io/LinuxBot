d9d971308b9a9955704e48fe79f61dcb59b20489
Josh <josh@opaliq.com>
2024-06-26 12:59:44 -0400

    corrected utils import

--- linuxbot.sh
+++ linuxbot.sh
@@ -3,3 +3,6 @@
 # Run the application (change path as needed)
 cd ~/_DEV/_INTERNAL/LinuxBot/app/
 python3 app.py
+
+# INSTRUCTIONS: Move linuxbot.sh to /usr/local/bin/ and make it executable
+# with the combined command: sudo cp linuxbot.sh /usr/local/bin/linuxbot.sh && sudo chmod +x /usr/local/bin/linuxbot.sh
