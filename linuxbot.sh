fd2efe9c5dd58d8562a85be0b7e8c0493378f953
Josh <josh@opaliq.com>
2024-06-27 12:50:26 -0400

    WORKING

--- linuxbot.sh
+++ linuxbot.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# # Update the system
+# sudo cp linuxbot.sh /usr/local/bin/linuxbot.sh && sudo chmod +x /usr/local/bin/linuxbot.sh
+
 # Run the application (change path as needed)
-cd ~/_DEV/_INTERNAL/LinuxBot/app/core/
+cd ~/_DEV/_INTERNAL/LinuxBot/app/core
+
+export PYTHONPATH=~/_DEV/_INTERNAL/LinuxBot/
+
 python3 run.py
