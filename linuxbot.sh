9e99ed09683a14eafe4c84433bd073aee15c5978
Josh <josh@opaliq.com>
2024-07-29 12:23:03 -0400

    removed commented code from sh file

--- linuxbot.sh
+++ linuxbot.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# # Update the system
-# sudo cp linuxbot.sh /usr/local/bin/linuxbot.sh && sudo chmod +x /usr/local/bin/linuxbot.sh
-
 # Run the application (change path as needed)
 export PYTHONPATH=~/_DEV/_INTERNAL/LinuxBot/
 cd ~/_DEV/_INTERNAL/LinuxBot/app/core
